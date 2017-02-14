79947d857c592542382da302bf62525bb4fbb05e
YLCHUN <985050622@qq.com>
2017-02-14 16:24:31 +0800

    no message

--- NCUException/NCUException/NSException+Signal.h
+++ NCUException/NCUException/NSException+Signal.h
@@ -18,3 +18,5 @@ FOUNDATION_EXPORT void NSSetSignalExceptionHandler(NSUncaughtExceptionHandler * 
 FOUNDATION_EXPORT NSUncaughtExceptionHandler * _Nullable NSGetAllExceptionHandler(void);
 FOUNDATION_EXPORT void NSSetAllExceptionHandler(NSUncaughtExceptionHandler * _Nullable);
 
+
+FOUNDATION_EXPORT void NSChangeUncaughtExceptionHandler(void (^ _Nonnull newHandler)(NSUncaughtExceptionHandler * _Nullable handler));//每(3/1000)秒内检测是否变化，有效时间3秒,执行一次
